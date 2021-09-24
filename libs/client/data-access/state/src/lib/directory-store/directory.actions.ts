f3bfea9f21d7fddc1c5dbf1328940c1d153a1151
klingebiel <lukas.klingebiel@web.de>
2021-09-24 15:48:00 +0200

    refactor(directory): remove unused actions

--- libs/client/data-access/state/src/lib/directory-store/directory.actions.ts
+++ libs/client/data-access/state/src/lib/directory-store/directory.actions.ts
@@ -3,18 +3,8 @@ import { Update } from "@ngrx/entity";
 
 import { Directory } from "./directory.model";
 
-export const loadDirectories = createAction(
-	"[Directory/API] Load Directories",
-	props<{ directories: Directory[] }>()
-);
-
 export const addDirectory = createAction(
 	"[Directory/API] Add Directory",
-	props<{ directory: Directory }>()
-);
-
-export const upsertDirectory = createAction(
-	"[Directory/API] Upsert Directory",
 	props<{ directory: Directory }>()
 );
 
@@ -23,29 +13,14 @@ export const addDirectories = createAction(
 	props<{ directories: Directory[] }>()
 );
 
-export const upsertDirectories = createAction(
-	"[Directory/API] Upsert Directories",
-	props<{ directories: Directory[] }>()
-);
-
 export const updateDirectory = createAction(
 	"[Directory/API] Update Directory",
 	props<{ directory: Update<Directory> }>()
 );
 
-export const updateDirectories = createAction(
-	"[Directory/API] Update Directories",
-	props<{ directories: Update<Directory>[] }>()
-);
-
 export const deleteDirectory = createAction(
 	"[Directory/API] Delete Directory",
 	props<{ directory: Directory }>()
-);
-
-export const deleteDirectories = createAction(
-	"[Directory/API] Delete Directories",
-	props<{ ids: string[] }>()
 );
 
 export const clearDirectories = createAction("[Directory/API] Clear Directories");
--- libs/client/data-access/state/src/lib/directory-store/directory.reducer.ts
+++ libs/client/data-access/state/src/lib/directory-store/directory.reducer.ts
@@ -17,29 +17,14 @@ export const initialState: State = adapter.getInitialState();
 export const reducer = createReducer(
 	initialState,
 	on(DirectoryActions.addDirectory, (state, action) => adapter.addOne(action.directory, state)),
-	on(DirectoryActions.upsertDirectory, (state, action) =>
-		adapter.upsertOne(action.directory, state)
-	),
 	on(DirectoryActions.addDirectories, (state, action) =>
 		adapter.addMany(action.directories, state)
-	),
-	on(DirectoryActions.upsertDirectories, (state, action) =>
-		adapter.upsertMany(action.directories, state)
 	),
 	on(DirectoryActions.updateDirectory, (state, action) =>
 		adapter.updateOne(action.directory, state)
 	),
-	on(DirectoryActions.updateDirectories, (state, action) =>
-		adapter.updateMany(action.directories, state)
-	),
 	on(DirectoryActions.deleteDirectory, (state, action) =>
 		adapter.removeOne(action.directory.path, state)
-	),
-	on(DirectoryActions.deleteDirectories, (state, action) =>
-		adapter.removeMany(action.ids, state)
-	),
-	on(DirectoryActions.loadDirectories, (state, action) =>
-		adapter.setAll(action.directories, state)
 	),
 	on(DirectoryActions.clearDirectories, (state): State => {
 		const emptyState = adapter.removeAll(state);

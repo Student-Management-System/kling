9f6f836e647c6f7fadf8b81ef6c6d65a6f8f7d4f
klingebiel <lukas.klingebiel@web.de>
2021-09-30 16:00:34 +0200

    test: remove empty test suites

--- libs/client/data-access/state/src/lib/workspace-store/test/workspace.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromWorkspace from "../workspace.actions";
-
-describe("loadWorkspaces", () => {
-	it("should return an action", () => {
-		expect(fromWorkspace.loadWorkspaces().type).toBe("[Workspace] Load Workspaces");
-	});
-});

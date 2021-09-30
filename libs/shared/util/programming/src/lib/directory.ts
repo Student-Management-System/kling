a8d88f0e459c7d9cdadbfc34dfbbeafb65049806
klingebiel <lukas.klingebiel@web.de>
2021-09-30 19:04:41 +0200

    test(directory): add tests

--- libs/shared/util/programming/src/lib/directory.ts
+++ libs/shared/util/programming/src/lib/directory.ts
@@ -1,3 +1,5 @@
+import { File } from "..";
+
 export interface Directory {
 	path: string;
 	parentDirectoryPath?: string;
@@ -10,4 +12,41 @@ export function createDirectory(name: string, parentDirectoryPath?: string): Dir
 		parentDirectoryPath,
 		name
 	};
+}
+
+/**
+ * Given a list of {@link File}s, creates and returns all occurring directories.
+ */
+export function createDirectoriesFromFiles(files: File[]): Directory[] {
+	const directorySet = new Set(
+		files.filter(f => f.directoryPath.length > 0).map(f => f.directoryPath)
+	);
+
+	const directoryMap = new Map<string, Directory>();
+
+	for (const path of directorySet) {
+		const directories = createDirectoriesFromPath(path);
+		directories.forEach(dir => directoryMap.set(dir.path, dir));
+	}
+
+	return [...directoryMap.values()];
+}
+
+/**
+ * Creates all directories that occur in the given `directoryPath`.
+ * @example
+ * const paths = createDirectoriesFromPath("one/two/three")
+ * 	.map(d => d.path);
+ * console.log(paths) // [one, one/two, one/two/three]
+ */
+export function createDirectoriesFromPath(directoryPath: string): Directory[] {
+	const directories: Directory[] = [];
+	const hierarchy = directoryPath.split("/");
+
+	for (let i = 0; i < hierarchy.length; i++) {
+		const directory = createDirectory(hierarchy[i], directories[i - 1]?.path);
+		directories.push(directory);
+	}
+
+	return directories;
 }

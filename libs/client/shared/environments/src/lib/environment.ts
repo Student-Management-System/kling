21b099b6688eaa1c46a45a8afd574e043a1608cb
klingebiel <lukas.klingebiel@web.de>
2021-10-13 17:57:14 +0200

    refactor(environments): remove unused variables

--- libs/client/shared/environments/src/lib/environment.ts
+++ libs/client/shared/environments/src/lib/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
 	production: false,
-	hmr: false,
-	API_BASE_PATH: "http://localhost:4000",
-	STUDENT_MGMT_BASE_PATH: "http://localhost:3000",
-	AUTH_BASE_PATH: "http://147.172.178.30:8080",
-	PISTON_CODE_EXECUTION_BASE_PATH: "http://localhost:2000"
+	hmr: false
 };

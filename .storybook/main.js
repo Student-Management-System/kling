77f50ee5744c1f50f7c4dca21a9c129b6d4b0980
klingebiel <lukas.klingebiel@web.de>
2021-11-15 16:22:16 +0100

    chore(storybook): add dark mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	stories: [],
-	addons: ["@storybook/addon-essentials", "storybook-tailwind-dark-mode"]
+	addons: ["@storybook/addon-essentials", "storybook-dark-mode", "storybook-tailwind-dark-mode"]
 	// uncomment the property below if you want to apply some webpack config globally
 	// webpackFinal: async (config, { configType }) => {
 	//   // Make whatever fine-grained changes you need that should apply to all storybook configs

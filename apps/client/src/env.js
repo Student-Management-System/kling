6e11bf7f4e8bac3ebf4661b7f0ad356dbd332cd5
klingebiel <lukas.klingebiel@web.de>
2022-01-25 19:24:03 +0100

    chore: run migrations

--- apps/client/src/env.js
+++ apps/client/src/env.js
@@ -4,5 +4,6 @@
 	window.__env.AUTH_BASE_PATH = "http://localhost:8080";
 	window.__env.EXERCISE_SUBMITTER_BASE_PATH = "http://localhost:8001";
 	window.__env.PISTON_CODE_EXECUTION_BASE_PATH = "http://localhost:2000";
-	window.__env.CONVERGENCE_REALTIME_API_PATH = "http://localhost:8000/api/realtime/convergence/default"
+	window.__env.CONVERGENCE_REALTIME_API_PATH =
+		"http://localhost:8000/api/realtime/convergence/default";
 })(this);
--- libs/shared/storybook/.storybook/preview.js
+++ libs/shared/storybook/.storybook/preview.js
@@ -1,3 +1,3 @@
 export const globalTypes = {
-	darkMode: true,
+	darkMode: true
 };
